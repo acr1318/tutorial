59300b1818acb001d5c33b880a68b132ef2e2a53
jake <jake645x@gmail.com>
2013-04-25 12:03:15 +0900

    주석추가

--- error.c
+++ error.c
@@ -1,6 +1,8 @@
 #include "apue.h"
 #include <errno.h>		/* for definition of errno */
 #include <stdarg.h>		/* ISO C variable aruments */
+/*SZXDCV*/
+
 
 static void	err_doit(int, int, const char *, va_list);
 
